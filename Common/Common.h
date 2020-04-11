2743aef62892a85e5f47e8027ac281781bbe5c69
cs-lucas-nascimento <l.b.do.nascimento@accenture.com>
2020-04-11 02:18:05 -0300

    Initial commit

---
+++ Common/Common.h
@@ -0,0 +1,19 @@
+//
+//  Common.h
+//  Common
+//
+//  Created by l.b.do.nascimento on 11/04/20.
+//  Copyright © 2020 lucas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Common.
+FOUNDATION_EXPORT double CommonVersionNumber;
+
+//! Project version string for Common.
+FOUNDATION_EXPORT const unsigned char CommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Common/PublicHeader.h>
+
+
