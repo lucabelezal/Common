fa65e9c2b802b771a7adb07b168cdb6bb24f9c52
cs-lucas-nascimento <l.b.do.nascimento@accenture.com>
2020-05-24 13:09:09 -0300

    Add static SPM support

--- Package.swift
+++ Package.swift
@@ -1,31 +1,15 @@
 // swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "Common",
-        platforms: [
-        .iOS(.v11)
-    ],
+    platforms: [.iOS(.v11)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Common",
-            targets: ["Common"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .library(name: "Common", targets: ["Common"]),
+        .library(name: "CommonStatic", type: .static, targets: ["Common"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Common",
-            dependencies: []),
-        .testTarget(
-            name: "CommonTests",
-            dependencies: ["Common"]),
+        .target(name: "Common"),
+        .testTarget(name: "CommonTests", dependencies: ["Common"])
     ]
 )
